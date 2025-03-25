a2158d491de646d94bf64b7a84ab4a7af1c9ba72
wangxiuwen <wangxiuwen@wangxiuwen.com>
2025-03-25 12:05:46 +0800

    fix: language

--- src/asr/whisper_cpp_asr.py
+++ src/asr/whisper_cpp_asr.py
@@ -9,18 +9,19 @@ class VoiceRecognition(ASRInterface):
     def __init__(
         self,
         model_name: str = "base",
-        model_dir="asr/models",
-        language: str = "en",
-        print_realtime=False,
-        print_progress=False,
+        model_dir="models",
+        language: str = None,
+        print_realtime=True,
+        print_progress=True,
         **kwargs,
     ) -> None:
         self.model = Model(
             model=model_name,
             models_dir=model_dir,
-            language=language,
+            language= self.LANG if self.LANG else None,
             print_realtime=print_realtime,
             print_progress=print_progress,
+            debug=True,
             **kwargs,
         )
 
